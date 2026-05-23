0931b4cfb5d0828018c8537de3773b567ed3f45b
Ahnaf Anan <aanana0171@gmail.com>
2026-05-23 16:07:19 +0600

    app.php modified

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Middleware\EnsureAdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +15,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role' => RoleMiddleware::class,
+            'permission' => PermissionMiddleware::class,
+            'admin' => EnsureAdminMiddleware::class
+        ]);
+
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
