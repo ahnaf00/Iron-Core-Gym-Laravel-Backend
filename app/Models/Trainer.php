de226004ccaa86ab4343fbd4336a51d879533ba7
Ahnaf Anan <aanana0171@gmail.com>
2026-05-23 11:19:39 +0600

    trainer model modified

--- app/Models/Trainer.php
+++ app/Models/Trainer.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Trainer extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $cats = [
+        'is_active' => 'boolean'
+    ];
+
+    public function classes()
+    {
+        return $this->hasmany(ClassSession::class);
+    }
+
+    public function getPgotoUrlAttribute()
+    {
+        return $this->photo ? Storage::url($this->photo):null;
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active',true);
+    }
 }
