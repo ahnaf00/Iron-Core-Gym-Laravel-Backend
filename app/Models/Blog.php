f6f7fa647e7a6a65240cb510b33a337650cc10c6
Ahnaf Anan <aanana0171@gmail.com>
2026-05-23 11:27:34 +0600

    Blog model modified

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,8 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Blog extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'tags'          => 'array',
+        'is_published'  => 'boolean',
+        'published_at'  => 'datetime'
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function getThumbnailUrlattribute()
+    {
+        return $this->thumbnail ? Storage::url($this->thumbnail):null;
+    }
+
+    public function scopePublished($query)
+    {
+        return $query->where('is_published',true);
+    }
 }
