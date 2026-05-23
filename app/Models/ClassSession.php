8b51ee46392be26f10ddb978ec59643d6e9ef894
Ahnaf Anan <aanana0171@gmail.com>
2026-05-23 11:22:30 +0600

    ClassSession Model modified

--- app/Models/ClassSession.php
+++ app/Models/ClassSession.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ClassSession extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'is_active' => 'boolean'
+    ];
+
+    public function trainer()
+    {
+        return $this->belongsTo(Trainer::class);
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active',true);
+    }
 }
