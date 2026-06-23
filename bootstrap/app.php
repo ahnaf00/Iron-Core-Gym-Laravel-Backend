2376146abcce3f713e8bc9d80914a66a1f2aa288
Ahnaf Anan <aanana0171@gmail.com>
2026-06-23 12:39:02 +0600

    by passing csrf protection for testing api endpoints

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->statefulApi();
+
+        $middleware->preventRequestForgery(except:[
+            "*"
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
